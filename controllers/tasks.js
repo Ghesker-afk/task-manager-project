787d0192abffab5cce04fba362014b1f4387e2fa
Ghesker-afk <spacexz12345@gmail.com>
2025-09-03 23:36:34 -0300

    feat(controllers/tasks.js): implemented the rest of CRUD operations in the controllers updateTask and deleteTask.

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -33,12 +33,33 @@ async function getTask(req, res) {
   }
 }
 
-function updateTask(req, res) {
-  res.send('update task');
+async function updateTask(req, res) {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, { new: true });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({msg: error});
+  }
 }
 
-function deleteTask(req, res) {
-  res.send('delete task');
+async function deleteTask(req, res) {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({msg: `No task with id: ${taskID}`});
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 }
 
 module.exports = {
