d9cc7c2cd726b75fdf31b4d5e30396f8f2ec0f1c
Ghesker-afk <spacexz12345@gmail.com>
2025-09-03 23:45:16 -0300

    feat(*): added the rest of the folders that has been used as the frontend, such as public.

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+
+// middleware 
+
+app.use(express.json());
+
+// routes
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App');
+});
+
+app.use(express.static('./public'));
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+async function start() {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+start();
